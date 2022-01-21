30f90fefe70b63850304c5262cd91bd74479e81f
Manuel Patlan <patlanio@outlook.com>
2022-01-21 17:55:14 -0600

    Add sample React view

---
+++ app/javascript/components/AddressForm.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class AddressForm extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Dirección</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AddressForm
