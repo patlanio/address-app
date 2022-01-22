f6693f67d961c03a609a9bdc157574008119487b
Manuel Patlan <patlanio@outlook.com>
2022-01-21 18:47:18 -0600

    Add placeholder data to Address form

--- app/javascript/components/AddressForm.js
+++ app/javascript/components/AddressForm.js
@@ -6,6 +6,16 @@ class AddressForm extends React.Component {
     return (
       <React.Fragment>
         <h1>Dirección</h1>
+        <ul>
+          <li>street: { this.props.street }</li>
+          <li>ext_num: { this.props.ext_num }</li>
+          <li>int_num: { this.props.int_num }</li>
+          <li>zipcode: { this.props.zipcode }</li>
+          <li>neighborhood: { this.props.neighborhood }</li>
+          <li>city: { this.props.city }</li>
+          <li>state: { this.props.state }</li>
+          <li>country: { this.props.country }</li>
+        </ul>
       </React.Fragment>
     );
   }
