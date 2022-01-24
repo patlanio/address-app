a2fe63b0a118c3e17d1a2472e859281682dcf39f
Manuel Patlan <patlanio@outlook.com>
2022-01-24 13:01:53 -0600

    Add address creation endpoint

--- app/controllers/v1/addresses_controller.rb
+++ app/controllers/v1/addresses_controller.rb
@@ -6,6 +6,20 @@ class V1::AddressesController < ApplicationController
     render json: @addresses
   end
 
+  def create
+    permitted_params = params.permit(:street, :ext_num, :int_num, :neighborhood, :city, :state, :zipcode, :country)
+    @country = Country.find_by_name(permitted_params[:country])
+
+    address = Address.new(permitted_params.except(:country))
+    address.country = @country
+
+    if address.save!
+      render json: address
+    else
+      render json: address.errors, status: :bad_request
+    end
+  end
+
   def neighborhoods
     permitted_params = params.permit(:country, :zipcode)
     neighborhoods = ::NeighborhoodsService.get(permitted_params[:country], permitted_params[:zipcode])
