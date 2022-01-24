abe237de2e115f9d961e88358570436a07da9b71
Manuel Patlan <patlanio@outlook.com>
2022-01-23 23:00:27 -0600

    Expose neighborhood through API

--- app/controllers/v1/addresses_controller.rb
+++ app/controllers/v1/addresses_controller.rb
@@ -5,4 +5,10 @@ class V1::AddressesController < ApplicationController
     @addresses = Address.all
     render json: @addresses
   end
+
+  def neighborhoods
+    permitted_params = params.permit(:country, :zipcode)
+    neighborhoods = ::NeighborhoodsService.get(permitted_params[:country], permitted_params[:zipcode])
+    render json: neighborhoods
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   namespace :v1, defaults: { format: 'json'} do
     get 'countries', to: 'countries#index'
-    resources :addresses
+    resources :addresses do
+      get :neighborhoods, on: :collection
+    end
   end
 end
