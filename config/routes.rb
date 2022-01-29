83045ee5b99488d131bd621d5a22d9233c18083d
Manuel Patlan <patlanio@outlook.com>
2022-01-29 11:53:31 -0600

    Redirect all non-api request to main static index controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  root 'static#index'
-
   namespace :v1, defaults: { format: 'json'} do
     get 'countries', to: 'countries#index'
     resources :addresses do
       get :neighborhoods, on: :collection
     end
   end
+
+  get '*page', to: 'static#index', constrains: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
 end
