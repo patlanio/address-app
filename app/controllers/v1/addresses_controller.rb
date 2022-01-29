d0c30c605aa6564aaf93b1dfe7060213e66fc43b
Manuel Patlan <patlanio@outlook.com>
2022-01-29 12:35:42 -0600

    Improve REST api methods

--- app/controllers/v1/addresses_controller.rb
+++ app/controllers/v1/addresses_controller.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
 class V1::AddressesController < ApplicationController
+  before_action :set_address, only: %i[show update destroy]
+
   def index
     @addresses = Address.all
     render json: @addresses
   end
 
-  def create
-    permitted_params = params.permit(:street, :ext_num, :int_num, :neighborhood, :city, :state, :zipcode, :country)
-    @country = Country.find_by_name(permitted_params[:country])
+  def show
+    render json: @address
+  end
 
-    address = Address.new(permitted_params.except(:country))
+  def create
+    @country = Country.find_by_name(address_params[:country])
+
+    address = Address.new(address_params.except(:country))
     address.country = @country
 
     if address.save!
@@ -20,9 +25,35 @@ class V1::AddressesController < ApplicationController
     end
   end
 
+  def update
+    @country = Country.find_by_name(address_params[:country])
+    @address.country = @country
+
+    if @address.save!
+      render json: @address
+    else
+      render json: @address.errors, status: :bad_request
+    end
+  end
+
+  def destroy
+    @address.destroy
+    head :no_content
+  end
+
   def neighborhoods
     permitted_params = params.permit(:country, :zipcode)
     neighborhoods = ::NeighborhoodsService.get(permitted_params[:country], permitted_params[:zipcode])
     render json: neighborhoods
+  end
+
+  private
+
+  def address_params
+    params.permit(:street, :ext_num, :int_num, :neighborhood, :city, :state, :zipcode, :country)
+  end
+
+  def set_address
+    @address = Address.find(params[:id])
   end
 end
